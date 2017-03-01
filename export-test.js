90eb893a5a6ba264041e9c697a1e1306c50d4cc7
Rasmus Jensen <moc.buhtig@solsort.dk>
2017-03-01 16:45:40 +0100

    Commit export-test.js via https://appedit.solsort.com/?page=edit&github=KodeKunstner/export-test

---
+++ export-test.js
@@ -0,0 +1,28 @@
+// # AppEdit Hello World
+//
+// Modules/libraries are loaded with `require(...)`.
+//
+// Documentation/source for the solsort library can be read on:
+// <https://appedit.solsort.com/?page=read&github=solsort/solsort>
+
+var ss = require('solsort');
+
+// Render the ui reactively
+
+ss.html(() => ['div',
+  ['h1', 'Hello world'],
+  ['p', 'Count: ', ss.getJS('count', 0)],
+  ['button', {onClick: ss.event('increment')},
+    'Increment']]);
+
+// Handler for button clicks
+
+ss.handle('increment', () => 
+  ss.setJS('count', ss.getJS('count', 0) + 1));
+
+// Information about the app, - used for exporting to github, etc.
+
+exports.info = {
+  name: 'Hello World',
+  github: 'kodekunstner/export-test'
+};
